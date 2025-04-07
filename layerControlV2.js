a797d1bd3f24b03a9ec445ba6fcb889eb4abbad9
nanano <68322325+nanano77@users.noreply.github.com>
2025-04-07 14:42:24 +0800

    Update layerControlV2.js

--- layerControlV2.js
+++ layerControlV2.js
@@ -114,12 +114,12 @@ export async function handleCommand(text, mode, updateUIFn) {
   }
 }
 
-async function queryGPT(userInput) {
+async function queryGPT(userInput, lastCandidates = []) {
   try {
-    const res = await fetch("/api/tt", {
+    const res = await fetch("/api/ttV2", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userInput })
+      body: JSON.stringify({ userInput, lastCandidates })
     });
     return await res.json();
   } catch (e) {
